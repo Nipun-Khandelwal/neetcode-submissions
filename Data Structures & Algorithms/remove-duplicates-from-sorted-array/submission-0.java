4d0c21e06f403af287fd315b004bfe9da06fc2d7
Nipun-Khandelwal <Nipun.23FE10CSE00268@muj.manipal.edu>
2026-08-23 14:52:52 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        TreeSet<Integer> set=new TreeSet<>();
+        for(int i=0;i<nums.length;i++){
+            set.add(nums[i]);
+        }
+        int i=0;
+        for(int ele:set){
+            nums[i]=ele;
+            i++;
+        }
+        return set.size();
+    }
+}
